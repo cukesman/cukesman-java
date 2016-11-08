0c60ee1fc045a729d80eb388d5e64f624ab49051
Jan Stamer <jan.stamer@red6-es.de>
2016-11-08 20:57:24 +0100

    Fixed encoding and json headers.

--- cukesman-reporter-common/src/main/java/cukesman/reporter/CukesmanReportAPI.java
+++ cukesman-reporter-common/src/main/java/cukesman/reporter/CukesmanReportAPI.java
@@ -1,11 +1,14 @@
 package cukesman.reporter;
 
 import cukesman.reporter.model.ExecutionReport;
+import feign.Headers;
 import feign.RequestLine;
 
+@Headers("Accept: application/json;charset=UTF-8")
 public interface CukesmanReportAPI {
 
     @RequestLine("POST /executions/report")
+    @Headers("Content-Type: application/json;charset=UTF-8")
     void reportExecution(final ExecutionReport executionReport);
 
 }
